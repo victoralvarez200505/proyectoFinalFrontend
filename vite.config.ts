4b626679ce9556dca9c03b249893d0e88397b8f4
Renzo <renzohpg@gmail.com>
2025-11-04 21:40:51 -0300

    Para GhPages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/proyectoFinalFrontend/', // Set base for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
